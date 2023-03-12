b842dd177a867c06295df0e482e57b3d336ddd2d
Desiree <delgado.becerra.desiree@gmail.com>
2023-03-12 03:49:15 -0500

    solucion a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,7 @@
+function hola(nombre) {
+    return "hola " + nombre+ "!";
+} 
+
+console.log(hola ("Desiree")) // "Hola Desiree!"
+console.log(hola ("Dani")) // "Hola Dani!"
+console.log(hola ("")) // "Hola !"
---
+++ js-1/sum.js
@@ -0,0 +1,11 @@
+function sum(number) {
+    let total = 0;
+    for(let i = 1; i <= number; i++) {
+        total += i;
+    }
+    return total;
+}
+
+console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+console.log(sum(10)) // 55
+console.log(sum(15)) // 120
